2468f98ba6266fc3dfd6e08426d2c1b7150b1f97
ninjas123 <aantiguajavier2019@bklawtech.com>
2016-03-31 20:27:57 +0000

    I added the things

--- starter_code/js/wall.js
+++ starter_code/js/wall.js
@@ -2,5 +2,13 @@ $(document).ready(function()
 {
 	$('#message').focus();
 
+	$("#post-btn").click(function(){
+		$("#msg").html("<p>" + $("#message").val() + "</p>")
+		var naruto = $("#temp").clone();
+		naruto.css("display", "block")
+		naruto.prependTo("#wall");
+	});
 	// YOUR CODE GOES HERE
 });
+
+
